5cca4edd5c644f62ec60c40383d1af02000a07b3
kky-ykk <93654194+kky-ykk@users.noreply.github.com>
2022-06-06 20:20:12 +0530

    Add files via upload

---
+++ Array/IndexEqlEle.java
@@ -0,0 +1,48 @@
+//find the elements whose value is equal to that of its index value ( Consider 1-based indexing ).
+
+import java.util.*;
+import java.io.*;
+
+public class IndexEqlEle {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            ArrayList<Integer> ans = new Solution().valueEqualToIndex(arr, n);
+            if (ans.size() == 0) {
+                System.out.println("Not Found");
+            } else {
+                for (Integer x : ans) {
+                    System.out.print(x + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    ArrayList<Integer> valueEqualToIndex(int arr[], int n) {
+        // code here
+        ArrayList<Integer> values=new ArrayList<Integer>();
+        for(Integer i=0;i<n;i++){
+            if(i+1==arr[i])
+                values.add(i+1);
+        }
+        return values;        
+    }
+}
