d9a62492dfffbc5f47c67ba25cd00cff60f34cde
kky-ykk <kiahanyadavkisssson@gmail.com>
2022-06-14 13:13:39 +0530

    1-string

--- String/string.java
+++ String/string.java
@@ -1,5 +1,24 @@
-class String{
-	public static void main(String [] argu){
-	System.out.println("string check");
-}
+//Given a non-empty sequence of characters str, return true if sequence is Binary, else return false
+class string
+{
+	boolean isBinary(String str)
+	{
+	  //Your code here
+// 	  for(int i=0;i<str.length();i++){
+// 	    if  (str.charAt(i)!='0' && str.charAt(i)!='1')
+// 	        return false;
+	      
+// 	  }
+// 	  return true;
+
+    //-----------or-------------------
+    char str1[]=str.toCharArray();
+     for(char c:str1 ){
+	    if  (c!='0' && c!='1')
+	        return false;
+	      	  }
+	  return true;
+
+
+	}
 }
